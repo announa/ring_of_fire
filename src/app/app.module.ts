73e6755fe42d3d8cb79a08fdbcea82aef4ed6195
announa <moupou@posteo.org>
2022-01-11 14:17:51 -0500

    moved mobile player panel to player-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { StartscreenComponent } from './startscreen/startscreen.component';
 import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button'; 
-import {MatIconModule} from '@angular/material/icon';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card'; 
 import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';  
 import { FormsModule } from '@angular/forms';
 import { ActionsComponent } from './actions/actions.component';
@@ -21,7 +21,8 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
+/* import { PlayerMobileComponent } from './player-mobile/player-mobile.component'; */
+import { EditPlayerComponent } from './edit-player/edit-player.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
     PlayerComponent,
     DialogAddPlayerComponent,
     ActionsComponent,
-    PlayerMobileComponent
+    /* PlayerMobileComponent, */
+    EditPlayerComponent
   ],
   imports: [
     BrowserModule,
